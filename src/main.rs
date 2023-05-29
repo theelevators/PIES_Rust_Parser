517f7e4e56f538ef89dd1d561d77047b5a02a3ce
theelevators <elevatorstest@gmail.com>
2023-05-28 23:50:10 -0700

    small pies parser in progress. got to parse a pies 7.2 successfully

---
+++ src/main.rs
@@ -0,0 +1,257 @@
+use anyhow::Error;
+// use odbc_api::{ ConnectionOptions, Environment, Cursor, buffers::TextRowSet, Connection };
+use xml::attribute::OwnedAttribute;
+// const BATCH_SIZE: usize = 5000;
+use std::fs::File;
+use std::io::{ BufReader, Read };
+
+use xml::reader::{ EventReader, XmlEvent };
+
+fn main() {
+    // let env = Environment::new().expect("Cannot Connect To Database.");
+
+    // let connection_string =
+    //     "
+    //     Driver={ODBC Driver 17 for SQL Server};\
+    //     Server=VENUS\\LEVARE_SQL_1;\
+    //     Trusted_Connection=yes;\
+    // ";
+
+    // let conn = env
+    //     .connect_with_connection_string(connection_string, ConnectionOptions::default())
+    //     .expect("Cannot Connect To Database.");
+
+    // let query = "exec VCdb.dbo.SearchMakeByYear @Year = ?;";
+
+    // let results = fetch_rows(conn, query).expect("Unable to retrieve data.");
+
+    // println!("{}", results)
+
+    let f_path = "D:\\Projects\\WORK\\PIES\\Gates.xml";
+
+    parse_xml(f_path).expect("No Work!");
+}
+
+// pub fn fetch_rows(conn: Connection, query: &str) -> Result<String, Error> {
+//     let year = 2007;
+//     match conn.execute(query, &year)? {
+//         Some(mut cursor) => {
+//             let mut writer = csv::Writer::from_writer(vec![]);
+//             let mut buffers = TextRowSet::for_cursor(BATCH_SIZE, &mut cursor, Some(4096))?;
+//             // Bind the buffer to the cursor. It is now being filled with every call to fetch.
+//             let mut row_set_cursor = cursor.bind_buffer(&mut buffers)?;
+
+//             while let Some(batch) = row_set_cursor.fetch()? {
+//                 for row_index in 0..batch.num_rows() {
+//                     let record = (0..batch.num_cols()).map(|col_index| {
+//                         batch.at(col_index, row_index).unwrap_or(&[])
+//                     });
+//                     writer.write_record(record)?;
+//                 }
+//             }
+//             Ok(String::from_utf8(writer.into_inner()?)?)
+//         }
+//         None => { Ok(String::from("Query came back empty. No output has been created.")) }
+//     }
+// }
+
+pub fn parse_xml(f_path: &str) -> Result<(), Error> {
+    let file = File::open(f_path)?;
+    let file = BufReader::new(file);
+    let entries = PiesXmlIterator::new(file).map(|x| x.unwrap());
+
+    let item_no = entries
+        .filter(|x| x.item_num != None)
+        .last()
+        .expect("Should have an item.")
+        .item_num
+        .expect("There should be a number here!");
+    println!("File Contains: {} Items", item_no);
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct PiesEntry {
+    tag: String,
+    item_num: Option<u32>,
+    parent: String,
+    segment: String,
+    attributes: Option<Vec<OwnedAttribute>>,
+    value: Option<String>,
+}
+pub enum Segment {
+    Off,
+    Header,
+    Item,
+    Mkt,
+    Price,
+    Trailer,
+}
+
+struct PiesXmlIterator<R: Read> {
+    parser: EventReader<R>,
+    depth: u32,
+    parent: Option<String>,
+    item_num: u32,
+    tag: Option<String>,
+    segment: Option<String>,
+    attributes: Option<Vec<OwnedAttribute>>,
+    content: Option<String>,
+    p_state: Segment,
+}
+
+impl<R: Read> PiesXmlIterator<R> {
+    pub fn new(xml: R) -> PiesXmlIterator<R> {
+        PiesXmlIterator {
+            parser: EventReader::new(xml),
+            depth: 0,
+            item_num: 0,
+            parent: None,
+            tag: None,
+            segment: None,
+            attributes: None,
+            content: None,
+            p_state: Segment::Off,
+        }
+    }
+}
+
+impl<R: Read> Iterator for PiesXmlIterator<R> {
+    type Item = xml::reader::Result<PiesEntry>;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.parser.next() {
+                Ok(XmlEvent::StartElement { name, attributes, .. }) => {
+                    self.depth += 1;
+                    self.parent = Some(name.local_name.clone());
+
+                    match self.p_state {
+                        Segment::Off => {
+                            if name.local_name.as_str() == "Header" {
+                                self.p_state = Segment::Header;
+                                continue;
+                            }
+                        }
+                        Segment::Header => {
+                            let segment = "Header";
+                            if name.local_name.as_str() == "PriceSheets" {
+                                self.p_state = Segment::Price;
+                                continue;
+                            }
+                            self.tag = Some(name.local_name);
+                            self.segment = Some(String::from(segment));
+                        }
+                        Segment::Price => {
+                            let segment = "Price";
+                            if name.local_name.as_str() == "MarketingCopy" {
+                                self.p_state = Segment::Mkt;
+                                continue;
+                            }
+                            self.tag = Some(name.local_name);
+                            self.attributes = Some(attributes);
+                            self.segment = Some(String::from(segment));
+                        }
+                        Segment::Mkt => {
+                            let segment = "MarketingCopy";
+                            if name.local_name.as_str() == "Items" {
+                                self.p_state = Segment::Item;
+                                continue;
+                            }
+                            self.tag = Some(name.local_name);
+                            self.attributes = Some(attributes);
+                            self.segment = Some(String::from(segment));
+                        }
+                        Segment::Item => {
+                            let segment = "Items";
+                            if name.local_name.to_lowercase().as_str() == "item" {
+                                self.item_num += 1;
+                            }
+                            if name.local_name.as_str() == "Trailer" {
+                                self.p_state = Segment::Trailer;
+                                self.segment = Some(String::from("Trailer"));
+                                continue;
+                            }
+                            self.tag = Some(name.local_name);
+                            self.attributes = Some(attributes);
+                            self.segment = Some(String::from(segment));
+                        }
+                        Segment::Trailer => {
+                            self.segment = Some(String::from("Trailer"));
+                            self.tag = Some(name.local_name);
+                            self.attributes = Some(attributes);
+                        }
+                    }
+                }
+                Ok(XmlEvent::EndElement { name }) => {
+                    self.depth -= 1;
+                    self.parent = Some(name.local_name.clone());
+                    match self.p_state {
+                        Segment::Off => {}
+                        Segment::Header => {
+                            if self.depth < 2 {
+                                continue;
+                            }
+                            let out = PiesEntry {
+                                tag: self.tag.take().unwrap_or(name.local_name.clone()),
+                                item_num: None,
+                                parent: self.parent.take().unwrap(),
+                                segment: self.segment.clone().unwrap(),
+                                attributes: None,
+                                value: self.content.take(),
+                            };
+                            return Some(Ok(out));
+                        }
+                        Segment::Item => {
+                            if self.depth < 2 {
+                                continue;
+                            }
+                            let out = PiesEntry {
+                                tag: self.tag.take().unwrap_or(name.local_name.clone()),
+                                item_num: Some(self.item_num),
+                                parent: self.parent.take().unwrap(),
+                                segment: self.segment.clone().unwrap(),
+                                attributes: self.attributes.take(),
+                                value: self.content.take(),
+                            };
+                            return Some(Ok(out));
+                        }
+                        _ => {
+                            if self.depth < 2 {
+                                continue;
+                            }
+                            let out = PiesEntry {
+                                tag: self.tag.take().unwrap_or(name.local_name.clone()),
+                                item_num: None,
+                                parent: self.parent.take().unwrap(),
+                                segment: self.segment.clone().unwrap_or(name.local_name),
+                                attributes: self.attributes.take(),
+                                value: self.content.take(),
+                            };
+                            return Some(Ok(out));
+                        }
+                    }
+                }
+                Ok(XmlEvent::Characters(s)) => {
+                    match self.p_state {
+                        Segment::Off => {}
+                        _ => {
+                            self.content = Some(s);
+                        }
+                    }
+                }
+                Ok(XmlEvent::EndDocument { .. }) => {
+                    break;
+                }
+                Err(ref e) if e.kind() == &xml::reader::ErrorKind::UnexpectedEof => {
+                    break;
+                }
+                Err(e) => {
+                    return Some(Err(e));
+                }
+                _ => {}
+            }
+        }
+        None
+    }
+}
---
+++ src/pies_import_model.rs
@@ -0,0 +1,155 @@
+
+pub struct PIESHeader {
+    pies_version: String,
+    sub_type: String,
+    blanket_effect_date: String,
+    changes_since_date: String,
+    parent_duns_number: String,
+    parent_gln: String,
+    parent_vmrsid: String,
+    parent_aaiaid: String,
+    brand_owner_duns: String,
+    brand_owner_gln: String,
+    brand_owner_vmrsid: String,
+    brand_owner_aaiaid: String,
+    buyer_duns: String,
+    currency_code: String,
+    lang_code: String,
+    tech_contact: String,
+    contact_email: String,
+    pcdb_ver_date: String,
+    padb_ver_date: String,
+}
+
+pub struct PriceSheets {
+    sheets: Vec<PriceSheet>,
+}
+
+pub struct PriceSheet {
+    price_sheet: XmlContent,
+    price_sheet_name: String,
+    super_price_sheet_number: String,
+    currency_code: String,
+    price_zone: String,
+    effective_date: String,
+    exp_date: String,
+}
+
+pub struct MarketCopy {
+    market_copy_cont: XmlContent,
+    digital_assets: Vec<DigitalFileInfo>,
+}
+
+pub struct Product {
+    item: XmlCouple,
+    hazard_mat_code: String,
+    base_item_id: String,
+    item_lvl_gtin: XmlContent,
+    part_number: String,
+    brand_aaiaid: String,
+    brand_label: String,
+    vmrs_brand_id: String,
+    qty_per_app: XmlContent,
+    item_effective_date: String,
+    avail_date: String,
+    min_order_qty: XmlContent,
+    mfg_prod_codes: XmlContent,
+    aaia_prod_cat_code: String,
+    unspsc: String,
+    part_terminology_id: String,
+    vmrs_code: String,
+    descriptions: Vec<XmlContent>,
+    price: Vec<Price>,
+    expi: Vec<XmlContent>,
+    attributes: Vec<XmlContent>,
+    package: Vec<Package>,
+    kits: Vec<Kits>,
+    interchage: Vec<PartInterchange>,
+    digital_assets: Vec<DigitalFileInfo>,
+}
+
+pub struct Trailer {
+    item_count: String,
+    trans_date: String,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct XmlContent {
+    attributes: Vec<XmlCouple>,
+    value: String,
+}
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct XmlCouple {
+    name: String,
+    value: String,
+}
+
+pub struct PartInterchange {
+    interchange_info: XmlContent,
+    part_number: XmlContent,
+}
+
+pub struct DigitalFileInfo {
+    file_name: String,
+    asset_type: String,
+    file_type: String,
+    representation: String,
+    file_size: String,
+    resolution: String,
+    color_mode: String,
+    background: String,
+    orientation_view: String,
+    asset_dimension: XmlContent,
+    asset_dates: XmlContent,
+    country: String,
+}
+pub struct Kits {
+    comp_part_number: XmlContent,
+    comp_brand: String,
+    comp_brand_label: String,
+    comp_sub_brand: String,
+    com_sub_brand_label: String,
+    description: XmlContent,
+    qty_in_kit: XmlContent,
+    seq_code: String,
+    sold_sep: String,
+}
+pub struct Price {
+    pricing: XmlContent,
+    price_sheet_number: String,
+    currency_code: String,
+    effective_date: String,
+    exp_date: String,
+    price: XmlContent,
+    price_break: XmlContent,
+}
+
+pub struct ConsumerPackage {
+    pack: XmlContent,
+    pack_lvl_gtin: String,
+    pack_bar_code_chars: String,
+    pack_uom: String,
+    qty_of_eaches: String,
+    dimensions: PackDimensions,
+    weights: PackWeights,
+    weight_variance: String,
+}
+
+pub struct PackDimensions {
+    merch_h: String,
+    merch_w: String,
+    merch_l: String,
+    ship_h: String,
+    ship_w: String,
+    ship_l: String,
+}
+
+pub struct PackWeights {
+    weight: String,
+    dim_weight: String,
+}
+
+pub enum Package {
+    ConsumerPackage,
+    RegulatedPackage,
+}
